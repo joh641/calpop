6be2883f591c1a907608b1e2715e001c87847975
Joshua Lam <joh641@gmail.com>
2013-12-11 22:36:41 -0800

    api call to make sections

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,27 @@
 class Section < ActiveRecord::Base
 
+  include SplitTimes
+
   belongs_to :class
   has_and_belongs_to_many :timeslots
 
+  def self.make_section(building, population, days, start_time, end_time)
+    section = Section.new
+    section.building = building
+    section.population = population
+    section.save
+    section.update_timeslots(days, start_time, end_time)
+  end
+
+  def update_timeslots(days, start_time, end_time)
+    times = split_times(start_time, end_time)
+    days.split("").each do
+      # if is day
+      times.each do
+        Timeslot.make_timeslot(#day, start)
+      end
+    end
+  end
+    
+    
 end
---
+++ lib/split_times.rb
@@ -0,0 +1,7 @@
+module SplitTimes
+
+  # splits into half hour time chunks from start to end
+  def split_times(start_time, end_time)
+  end
+
+end
