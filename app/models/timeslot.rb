21770d4e8d0c5c1d749c8a414d633a81e06594be
Joshua Lam <joh641@gmail.com>
2013-12-13 19:13:25 -0800

    edge case where time was less than 3 digits

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -4,18 +4,22 @@ class Timeslot < ActiveRecord::Base
 
   # makes a timeslot from the given input
   def self.make_timeslot(day, start_time)
+    start_time = start_time.to_s
+    if start_time.length < 4
+      start_time = "0" + start_time
+    end
     timeslot = find_timeslot(day, start_time)
     if not timeslot
       timeslot = Timeslot.new
       timeslot.day = day
-      timeslot.start_time = Time.strptime(start_time.to_s, "%H%M")
+      timeslot.start_time = Time.strptime(start_time, "%H%M")
       timeslot.save
     end
     return timeslot
   end
 
   def self.find_timeslot(day, start_time)
-    self.where("day = ?", day).where("start_time = ?", Time.strptime(start_time.to_s, "%H%M")).first
+    self.where("day = ?", day).where("start_time = ?", Time.strptime(start_time, "%H%M")).first
   end
 
   def find_buildings
