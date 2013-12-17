d01b31731d042a1d569187b5bcea166308ecf3c9
Joshua Lam <joh641@gmail.com>
2013-12-16 21:53:43 -0800

    cors test

--- config/application.rb
+++ config/application.rb
@@ -65,10 +65,5 @@ module Calpop
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => '*'
-    }
-
   end
 end
