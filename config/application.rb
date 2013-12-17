5590734494601423a747de73de26fbbcb1d5c30f
Joshua Lam <joh641@gmail.com>
2013-12-16 21:42:58 -0800

    moved headers to application.rb

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,11 @@ module Calpop
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => '*'
+    }
+
   end
 end
